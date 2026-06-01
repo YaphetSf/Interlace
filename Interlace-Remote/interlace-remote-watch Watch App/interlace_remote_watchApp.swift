385d97c47d9815c87b98032f7a4c264b31f1d116
ding <m4p@dingz.uk>
2026-06-01 12:37:14 +0100

    added watchOS as a target

---
+++ Interlace-Remote/interlace-remote-watch Watch App/interlace_remote_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  interlace_remote_watchApp.swift
+//  interlace-remote-watch Watch App
+//
+//  Created by Ding Zhong on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct interlace_remote_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
